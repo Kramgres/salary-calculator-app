eafc06635d81d58fccd5587284ed86f10f25e557
kramgres <q12856502>
2021-11-09 12:51:41 +0700

    add logo in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import cn from 'classnames'
 
 import {PATHS} from '../../constants/paths'
 import stylesLayout from '../../layouts/MainLayout/MainLayout.module.scss'
+import Logo from '../../assets/images/sibdev-logo.svg'
 
 import styles from './Header.module.scss'
 
@@ -12,8 +13,12 @@ const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className={cn(stylesLayout.content, styles.header__content)}>
+        <NavLink to={PATHS.root} className={styles.logo}>
+          <img src={Logo} alt="Logo"/>
+        </NavLink>
         <nav className={styles.nav}>
-          <NavLink to={PATHS.calculator} className={({isActive}) => cn(styles.nav__item, { [styles.nav__item_active]: isActive })}>Калькулятор</NavLink>
+          <NavLink to={PATHS.calculator}
+            className={({isActive}) => cn(styles.nav__item, {[styles.nav__item_active]: isActive})}>Калькулятор</NavLink>
         </nav>
         <NavLink to={PATHS.login} className={styles.login}>Войти</NavLink>
       </div>
