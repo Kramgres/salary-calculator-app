7a02aba2e1090fa77c3267f545b7039c64d48d4a
kramgres <q12856502>
2021-12-28 10:24:56 +0700

    add prettier, husky, lint-staged

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,51 +1,55 @@
-import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
-import cn from 'classnames'
-import {useDispatch, useSelector} from 'react-redux'
-import {Button} from 'antd'
+import React, { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'antd';
 
-import {getUserAuthorizedState, getUserInfoState} from 'src/store/auth/getters'
-import Logo from 'src/assets/images/sibdev-logo.svg'
-import stylesLayout from 'src/layouts/MainLayout/MainLayout.module.scss'
-import {PATHS} from 'src/constants/paths'
-import {logout} from 'src/services/auth/auth'
-import {clearUserStateAction} from 'src/store/auth/actions'
+import { getUserAuthorizedState, getUserInfoState } from 'src/store/auth/getters';
+import Logo from 'src/assets/images/sibdev-logo.svg';
+import stylesLayout from 'src/layouts/MainLayout/MainLayout.module.scss';
+import { PATHS } from 'src/constants/paths';
+import { logout } from 'src/services/auth/auth';
+import { clearUserStateAction } from 'src/store/auth/actions';
 
-import styles from './Header.module.scss'
-
+import styles from './Header.module.scss';
 
 const Header: FC = () => {
-  const isAuthorized = useSelector(getUserAuthorizedState)
-  const user = useSelector(getUserInfoState)
-  const dispatch = useDispatch()
+  const isAuthorized = useSelector(getUserAuthorizedState);
+  const user = useSelector(getUserInfoState);
+  const dispatch = useDispatch();
 
   const onLogout = () => {
-    logout()
-    dispatch(clearUserStateAction())
-
-  }
+    logout();
+    dispatch(clearUserStateAction());
+  };
 
   return (
     <header className={styles.header}>
       <div className={cn(stylesLayout.content, styles.header__content)}>
         <NavLink to={PATHS.root} className={styles.logo}>
-          <img src={Logo} alt="Logo"/>
+          <img src={Logo} alt="Logo" />
         </NavLink>
         <nav className={styles.nav}>
-          <NavLink to={PATHS.calculator}
-            className={({isActive}) => cn(styles.nav__item, {[styles.nav__item_active]: isActive})}>Калькулятор</NavLink>
+          <NavLink
+            to={PATHS.calculator}
+            className={({ isActive }) => cn(styles.nav__item, { [styles.nav__item_active]: isActive })}
+          >
+            Калькулятор
+          </NavLink>
         </nav>
         {isAuthorized ? (
           <>
             <div className={styles.email}>{user?.email}</div>
             <Button onClick={onLogout}>Выйти</Button>
           </>
-        ): (
-          <NavLink to={PATHS.login} className={styles.login}>Войти</NavLink>
+        ) : (
+          <NavLink to={PATHS.login} className={styles.login}>
+            Войти
+          </NavLink>
         )}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
